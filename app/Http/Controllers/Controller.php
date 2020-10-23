3db205d5dfa70b6b5b7f015e96eddf8688bd3775
Tomas Navarro <tomas-navarro@hotmail.com>
2020-10-23 06:37:53 -0500

    simple quote update/

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,12 +7,12 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     public static $quotes = array(
-        "The Black Knight Always Triumphs! - Monty Python",
-        "Anyone who has never made a mistake has never tried anything new - Albert Einstein",
-        "Never Stop Exploring- The North Face",
-        "Be yourself; everyone else is already taken - Oscar Wilde",
-        "So many books, so little time - Frank Zappa",
-        "Be the change that you wish to see in the world - Mahatma Gandhi",
+        "The Black Knight Always Triumphs! - Monty Python",
+        "Anyone who has never made a mistake has never tried anything new -Albert Einstein",
+        "Never Stop Exploring - The North Face",
+        "Be yourself; everyone else is already taken - Oscar Wilde",
+        "So many books, so little time - Frank Zappa",
+        "Be the change that you wish to see in the world - Mahatma Gandhi",
     );
 
     public function index()
